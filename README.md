fb995bb4b120aa5767afb5c3238b25adb911af23
COHL7500 <cohl7500@gmail.com>
2021-04-18 12:28:53 +0200

    add base content

---
+++ README.md
@@ -0,0 +1,13 @@
+# qreps-bot
+
+This bot was developed by cohl7500 or The Cohl Era#9947 for the Discord server QualityReps. 
+
+// GENERAL INFO // 
+
+The bot is - to put it concisely - intended for gathering particular information from Taobao, and
+display it to fellow users on the Discord for the purpose of a more convenient experience of sharing
+Taobao links. 
+
+Whenever a link is received, the bot will display convenient information to the user, such as
+name, picture, price and seller; all of it translated. If desired, the bot could replace the current
+Taobao Converter bot, and perform the same conversion, thus it could be deemed an upgrade. 
---
+++ main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env pypy
+
+from dotenv import load_dotenv
+import os
+import discord
+from bs4 import BeautifulSoup
+from google_trans_new import google_translator
+import requests
+
+load_dotenv('config.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+GUILD = client.get_guild(id)
+
+
+def tb_scanner(link, author):
+    translator, page = google_translator(), requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    title = soup.find('div', id="J_Title")  # title.h3.text
+    price = soup.find('strong', id="J_StrPrice")  # price.text
+    shopname = soup.find('div', class_="tb-shop-info-wrap")  # shopname.strong.text
+    thumbnail = soup.find('img')['src']
+
+    # if len(title.h3.text) < 60:
+    response = discord.Embed(
+        url=link,
+        title=translator.translate(title.h3.text, lang_src='ch', lang_tgt='en'),
+        color=0x00ff00
+    )
+
+    response.set_thumbnail(url="https:" + thumbnail)
+
+    response.add_field(
+        name="Price",
+        value=price.text,
+        inline=True
+    )
+
+    response.add_field(
+        name="Seller",
+        value=translator.translate(shopname.strong.text.strip(), lang_src='ch', lang_tgt='en'),
+        inline=True
+    )
+
+    response.set_author(
+        name=author
+    )
+
+    return response
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'https://item.taobao.com/item.htm' or 'item.taobao.com/item':
+        split = message.content.split()
+        links_found = []
+        for i in range(len(split)):
+            if 'https://item.taobao.com/item.htm' in split[i]:
+                links_found.append(tb_scanner(split[i], message.author))
+
+                if len(links_found) == len(split): # only Link
+                    for links in range(len(links_found)):
+                        await message.channel.send(embed=links_found[links])
+                    await message.delete()
+
+                elif len(links_found) < len(split): # w/ Text
+                    for links in range(len(links_found)):
+                        await message.channel.send(embed=links_found[links])
+
+
+client.run(TOKEN)
+
+# "https://item.taobao.com/item.htm"
+# "item.taobao.com/item"
